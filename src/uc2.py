2f2201d4e8d6060cc8a9ca83f59c7cfba4c9f7b6
BL-Yeswanth <yeswanth.polisetti@bridgelabz.com>
2026-01-05 16:43:37 +0530

    [yeswanth] Added:UC1:Add another Contact using Person class

---
+++ src/uc2.py
@@ -0,0 +1,13 @@
+class Person:
+    """
+    Represents a contact person
+    """
+    def __init__(self, first_name, last_name, address, city, state, zip_code, phone, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.phone = phone
+        self.email = email
